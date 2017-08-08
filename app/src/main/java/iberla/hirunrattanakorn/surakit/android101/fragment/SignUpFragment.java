ccb3e49e665304c405e3290082b194461d00400a
Menn3000 <Menn3000@hotmail.com>
2017-08-08 15:55:23 +0700

    Finish 8 Aug

---
+++ app/src/main/java/iberla/hirunrattanakorn/surakit/android101/fragment/SignUpFragment.java
@@ -0,0 +1,26 @@
+package iberla.hirunrattanakorn.surakit.android101.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import iberla.hirunrattanakorn.surakit.android101.R;
+
+/**
+ * Created by Menn on 8/8/2560.
+ */
+
+public class SignUpFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        return view;
+
+
+    }
+}//Main Class
