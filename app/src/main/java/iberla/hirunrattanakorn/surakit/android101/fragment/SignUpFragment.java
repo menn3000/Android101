cb09e6b39a88c014180bedf40162cdba83350de9
Menn3000 <Menn3000@hotmail.com>
2017-08-10 11:54:19 +0700

    Choose Picture Success

--- app/src/main/java/iberla/hirunrattanakorn/surakit/android101/fragment/SignUpFragment.java
+++ app/src/main/java/iberla/hirunrattanakorn/surakit/android101/fragment/SignUpFragment.java
@@ -1,5 +1,9 @@
 package iberla.hirunrattanakorn.surakit.android101.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -20,11 +24,11 @@ import iberla.hirunrattanakorn.surakit.android101.R;
 public class SignUpFragment extends Fragment {
 
     //Explicit
-    private ImageView backImageView, saveImageView, uploadImageView;
+    private ImageView backImageView, saveImageView, uploadImageView, pictureImageView;
     private EditText nameEditText, userEditText, passwordEditText;
     private String nameString, userString, passwordString;
     private String tag = "10augV1";
-
+    private Uri uri;
 
     @Nullable
     @Override
@@ -50,8 +54,59 @@ public class SignUpFragment extends Fragment {
         //Save Controller
         saveController();
 
+        //Picture Controller
+        pictureController();
+
 
     } //onActivityCreated
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode,
+                                 Intent data) {
+        // event fire after activity result
+        // requestCode is the Tag we passed as tag
+        // resultCode is status success or failed
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK ) {
+            Log.d(tag,"Result_OK");
+            uri = data.getData();
+
+            //Show Image
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity()
+                        .getContentResolver()
+                        .openInputStream(uri));
+                pictureImageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                Log.d(tag, "e Show Image ==> " + e.toString());
+            }//try
+
+
+
+
+        } //if
+
+
+
+    }//onActivityResult
+
+    private void pictureController() {
+        pictureImageView = getView().findViewById(R.id.imvPicture);
+        pictureImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Choose Picture
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*"); // ask user to select gallery program to work with
+                startActivityForResult(Intent.createChooser(intent,"Please choose app"),1); // 1 is tag , value type must be integer,  will be return as Request code
+
+            }//onClick
+        });
+    }
 
     private void saveController() {
         saveImageView.setOnClickListener(new View.OnClickListener() {
@@ -100,5 +155,7 @@ public class SignUpFragment extends Fragment {
         userEditText = getView().findViewById(R.id.edtUser);
         passwordEditText = getView().findViewById(R.id.edtPassword);
         uploadImageView = getView().findViewById(R.id.imvUpload);
-    }
+        pictureImageView = getView().findViewById(R.id.imvPicture);
+    }//initiealView
+
 }//Main Class
