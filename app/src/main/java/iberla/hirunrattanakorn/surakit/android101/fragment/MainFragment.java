200446f9a92f09829166ea793544be973bec5aff
Menn3000 <Menn3000@hotmail.com>
2017-08-10 09:53:54 +0700

    Create Back Controller

--- app/src/main/java/iberla/hirunrattanakorn/surakit/android101/fragment/MainFragment.java
+++ app/src/main/java/iberla/hirunrattanakorn/surakit/android101/fragment/MainFragment.java
@@ -3,9 +3,14 @@ package iberla.hirunrattanakorn.surakit.android101.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.zip.Inflater;
 
@@ -15,12 +20,58 @@ import iberla.hirunrattanakorn.surakit.android101.R;
  * Created by Menn on 8/8/2560.
  */
 
-public class MainFragment extends Fragment{
+public class MainFragment extends Fragment {
+
+    //Explict
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       View view = inflater.inflate(R.layout.fragment_main, container,false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
+    }// on CreateView ทำหน้าที่ในการสร้าง หน้ากาก หรือ view ไปแปะที่ Activity
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //initial view
+        initialView();
+
+        //TextView Controller
+        textViewController();
+
+
+    } //onActivityCreated จะทำงาน พร้อม Activity เราจะเริ่มต้นการทำงานของ code ที่นี้
+
+    private void textViewController() {
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Move to SignUpFragment
+
+                Log.d("10augV1", "Click OK");
+
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.myContent, new SignUpFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }//onClick
+        });
     }
+
+    private void initialView() {
+        userEditText = getView().findViewById(R.id.edtUser);
+        passwordEditText = getView().findViewById(R.id.edtPassword);
+        textView = getView().findViewById(R.id.txtNewRegister);
+        button = getView().findViewById(R.id.btnSignin);
+    }
+
+
 } //Main Class
--- app/src/main/java/iberla/hirunrattanakorn/surakit/android101/fragment/SignUpFragment.java
+++ app/src/main/java/iberla/hirunrattanakorn/surakit/android101/fragment/SignUpFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import iberla.hirunrattanakorn.surakit.android101.R;
 
@@ -15,16 +17,55 @@ import iberla.hirunrattanakorn.surakit.android101.R;
 
 public class SignUpFragment extends Fragment {
 
+    //Explicit
+    private ImageView backImageView, saveImageView;
+    private EditText nameEditText, userEditText, passwordEditText;
+
+
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
         return view;
 
+    }//onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Initieal View
+
+        initiealView();
+
+        //Back controller
+        backController();
 
 
+    } //onActivityCreated
 
+    private void backController() {
+        backImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                //Back to Main Fragment
+                getActivity().getSupportFragmentManager()
+                       .popBackStack(); // back to the last back stack , kill me
+
+            }//onClick
+        });
+    }
+
+    private void initiealView() {
+        backImageView = getView().findViewById(R.id.imvBack);
+        saveImageView = getView().findViewById(R.id.imgSave);
+        nameEditText = getView().findViewById(R.id.edtName);
+        userEditText = getView().findViewById(R.id.edtUser);
+        passwordEditText = getView().findViewById(R.id.edtPassword);
 
     }
 }//Main Class
